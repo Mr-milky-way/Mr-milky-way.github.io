32fac05905f9286697fb03b339233fbcccc28ec7
Mr-milky-way <admilkyway@gmail.com>
2025-04-10 07:43:59 -0700

    GPSguessr

--- content/js/gpsguessr.js
+++ content/js/gpsguessr.js
@@ -7,30 +7,42 @@ function getRandomArbitrary(min, max) {
 function GetLatAndLong() {
     la = getRandomArbitrary(-90, 90);
     lon = getRandomArbitrary(-90, 90);
+    GetLoc(la, lon);
     document.getElementById("lat").innerHTML = "Lat = " + la;
     document.getElementById("long").innerHTML = "Long = " + lon;
-    GetLoc(la, lon);
 }
 
 
-// Define the API URL
 const apiUrl = 'http://api.geonames.org/countryCodeXML?';
 
-// Make a GET request
 function GetLoc(lat, long) {
-    fetch(apiUrl + "lat=" + lat + "&lng=" + long + "&username=mrmilkyway")
+    fetch(`${apiUrl}lat=${lat}&lng=${long}&username=mrmilkyway`)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
-            return response.json();
+            return response.text();
         })
-        .then(data => {
-            console.log(data);
+        .then(xmlString => {
+            const parser = new DOMParser();
+            const xmlDoc = parser.parseFromString(xmlString, "application/xml");
+
+            const countryName = xmlDoc.querySelector("countryName")?.textContent;
+            const countryCode = xmlDoc.querySelector("countryCode")?.textContent;
+
+            if (countryName && countryCode) {
+                console.log("Country Name:", countryName);
+                console.log("Country Code:", countryCode);
+            } else {
+                console.warn("Could not extract country information from XML");
+                GetLatAndLong();
+            }
         })
         .catch(error => {
             console.error('Error:', error);
-            GetLatAndLong();
+            if (typeof GetLatAndLong === 'function') {
+                GetLatAndLong();
+            }
             alert(error);
         });
 }
