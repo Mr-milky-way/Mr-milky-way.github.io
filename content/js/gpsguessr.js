dcc32861a8076e2df8a2c368aae87d798f935e5f
Mr-milky-way <131718510+Mr-milky-way@users.noreply.github.com>
2025-04-10 21:31:40 +0000

    added function

--- content/js/gpsguessr.js
+++ content/js/gpsguessr.js
@@ -1,17 +1,26 @@
+let country = "";
+triesLeft = 2;
+numberRight = 0;
+
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-function GetLatAndLong() {
+async function GetLatAndLong() {
     la = getRandomArbitrary(-90, 90);
     lon = getRandomArbitrary(-180, 180);
-    GetLoc(la, lon);
-    document.getElementById("lat").innerHTML = "Lat = " + la;
-    document.getElementById("long").innerHTML = "Long = " + lon;
+    country = GetLoc(la, lon);
+    if (country === "ocean") {
+        document.getElementById("lat").innerHTML = "Lat = " + la;
+        document.getElementById("long").innerHTML = "Long = " + lon;
+    } else {
+        document.getElementById("lat").innerHTML = "Lat = " + la;
+        document.getElementById("long").innerHTML = "Long = " + lon;
+    }
 }
 
 
-const apiUrl = 'http://api.geonames.org/countryCodeXML?';
+const apiUrl = 'https://secure.geonames.org/countryCodeJSON?';
 
 function GetLoc(lat, long) {
     fetch(`${apiUrl}lat=${lat}&lng=${long}&username=mrmilkyway`)
@@ -19,24 +28,45 @@ function GetLoc(lat, long) {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
-            return response.text();
+            return response.json();
         })
-        .then(xmlString => {
-            const parser = new DOMParser();
-            const xmlDoc = parser.parseFromString(xmlString, "application/xml");
-
-            const countryName = xmlDoc.querySelector("countryName")?.textContent;
-            const countryCode = xmlDoc.querySelector("countryCode")?.textContent;
-
-            if (countryName && countryCode) {
-                console.log("Country Name:", countryName);
-                console.log("Country Code:", countryCode);
+        .then(data => {
+            if (data && data.countryName && data.countryCode) {
+                console.log("Country Name:", data.countryName);
+                console.log("Country Code:", data.countryCode);
+                countryName = data.countryName;
             } else {
                 console.log("In the ocean");
+                countryName = "ocean";
                 GetLatAndLong();
             }
         })
         .catch(error => {
             console.error('Error:', error);
         });
+    return countryName;
+}
+
+
+
+function guess() {
+    country = GetLoc(la, lon);
+    input = document.getElementById("input").value;
+    if (input.toLowerCase() === country.toLowerCase()) {
+        alert("Right On");
+        GetLatAndLong();
+        triesLeft = 2;
+        numberRight += 1;
+        document.getElementById("RightCounter").innerHTML = `Number Right: ${numberRight}`;
+    } else {
+        alert("Nope")
+        if (triesLeft <= 0) {
+            GetLatAndLong();
+            triesLeft = 2;
+            document.getElementById("GuessNumber").innerHTML = `Tries left: ${triesLeft + 1}`;
+        } else {
+            triesLeft -= 1;
+            document.getElementById("GuessNumber").innerHTML = `Tries left: ${triesLeft + 1}`;
+        }
+    }
 }
