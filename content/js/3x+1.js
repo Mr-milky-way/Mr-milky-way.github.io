9b10f7a6da998c8aae425ba723d694a719301fd6
Mr-milky-way <131718510+Mr-milky-way@users.noreply.github.com>
2025-04-26 17:28:15 +0000

    added Find button

--- content/js/3x+1.js
+++ content/js/3x+1.js
@@ -1,6 +1,8 @@
 let x = [1];
 let last = 1;
+let lastIt = 1
 let y = [1];
+let allNumbers = [];
 let highestdiff = 0;
 let highestdiffstartunmber = 0;
 
@@ -19,6 +21,7 @@ function start() {
     input = document.getElementById("input").value;
     y = [input];
     startnumber = input;
+    allNumbers.push(input);
     Run();
 }
 
@@ -28,6 +31,9 @@ function Run() {
     let current = parseInt(input);
     if (current === 1) {
         done = true;
+        if (lastIt < 100000) {
+            Find();
+        }
         return;
     } else if (isEven(current)) {
         current = current / 2;
@@ -35,7 +41,7 @@ function Run() {
         current = (current * 3) + 1
     }
 
-    if (current-startnumber > highestdiff){
+    if (current - startnumber > highestdiff) {
         highestdiff = current - startnumber;
         highestdiffstartunmber = startnumber;
     }
@@ -52,5 +58,17 @@ function Run() {
     }], {
         margin: { t: 0 }
     });
-    setTimeout(Run, 50);
+    if (allNumbers.includes(current)) {
+        Find();
+        return;
+    }
+    allNumbers.push(current);
+    setTimeout(Run, 1);
+}
+
+
+function Find() {
+    input = document.getElementById("input").value = lastIt;
+    lastIt += 1;
+    start();
 }
